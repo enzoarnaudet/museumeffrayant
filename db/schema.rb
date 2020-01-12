4ae3bb0a604b9ae3c1b376863af24837e2497db4
Enzo Arnaudet <aenzo09@hotmail.fr>
2020-01-12 15:43:24 +0100

    newww

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_092653) do
+ActiveRecord::Schema.define(version: 2020_01_09_154450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_01_08_092653) do
     t.string "dimensions"
     t.string "photo"
     t.string "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "firsts", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
